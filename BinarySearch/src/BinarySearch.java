927e9dd2221af342b850c8dc058cc4301b82e488
Joseph Cui <joeychui@icloud.com>
2018-02-21 20:33:58 +0800

    usual

--- BinarySearch/src/BinarySearch.java
+++ BinarySearch/src/BinarySearch.java
@@ -4,8 +4,12 @@ public class BinarySearch{
     public static void main(String[] args){
         int[] array = {1, 21, 3, 32, 43, 51,62};
         Arrays.sort(array);
+        for (int i:array) {
+            System.out.print(i + " ");
+        }
+        /*
         for(int i=0;i<array.length;i++){
             System.out.print(array[i] + " ");
-        }
+        }*/
     }
 }

7d6862a89bb000473dfed0c0324453a641fb6c65
Joseph Cui <joeychui@icloud.com>
2018-03-03 20:48:41 +0800

    add LinkedList

---
+++ ReplaceBlack/src/ReplaceBlack.java
@@ -0,0 +1,9 @@
+public class ReplaceBlack {
+    public static void main(String[] args){
+        StringBuffer str = new StringBuffer("sd sds sfs dolik");
+        System.out.println(str.toString());
+        System.out.println(str);
+    }
+
+    // return str.toString().replaceAll(" " , "%20");
+}
